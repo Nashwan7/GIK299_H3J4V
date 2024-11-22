8b072e964310464355eedb95b2b092e40c1d2f11
nashw <nashw@NASH>
2024-11-22 13:49:50 +0100

    Add project files.

---
+++ GIK299_H3J4V/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GIK299_H3J4V
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Väkommen till");
+            int AvgångstidNY_Timmar = 10;// Timme för avgångstid i New York
+            int AvgångstidNY_Minuter = 10;// Minuter för avgångstid i New York
+
+        }
+    }
+}
